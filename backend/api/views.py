c05aeedc57c6eca550f9d827d366d757d98a9bbf
Artur Z <arturziianbaev@gmail.com>
2025-05-14 18:06:49 -0700

    add login logout register views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,68 @@
 from django.shortcuts import render
-from rest_framework.response import Response
+from rest_framework.response import Response, sta
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
 # Create your views here.
 
 @api_view(['GET'])
 def home(request):
     return Response("home")
+
+
+ 
+
+# Authentication below
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        #TODO
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"You successfully created an account!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+class LoginLogoutView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return Response({"error":"username and password are required fields!"}, status=status.HTTP_400_BAD_REQUEST)
+        user =  authenticate(username=username, password=password)   
+        if not user or not user.is_active:
+            return Response({"error": "Invalid credentials or inactive account"}, status=status.HTTP_400_BAD_REQUEST)
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+        response = Response({
+            "message":"You logged in!", "username": user.username, "is_creator": user.is_creator
+            }, status=status.HTTP_200_OK)
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            max_age= 60*60*24*1,
+            secure=True,
+            httponly=True,
+            samesite="Lax"
+        )
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            max_age= 60*60*24*60,
+            secure=True,
+            httponly=True,
+            samesite="Lax"
+        )  
+        return response
+    def delete(self, request):
+        response = Response({"You logged out!"}, status=status.HTTP_200_OK)
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+        return response
